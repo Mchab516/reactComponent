eedbb11ff062b8aae5a0e3b9660a53198ecca67a
Mchab516 <mchab.mohammed@gmail.com>
2023-02-14 16:06:44 +0100

    Component

---
+++ src/Component/Header.js
@@ -0,0 +1,18 @@
+
+const Header = () => {
+  return (
+   <header>
+        <nav clasName = "navbar">
+        <h1>Hello World !</h1>
+        <div clasName = "links">
+        <a href = "/">Home </a>
+        <a href = "/create">New Post</a>
+
+        </div>
+
+        </nav>
+   </header>
+  )
+}
+
+export default Header
